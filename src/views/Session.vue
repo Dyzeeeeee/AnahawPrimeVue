2d7801c5a9d8b5852ed17d0a85cab6121c168d04
akoitosielcid <mamanaoelcid@gmail.com>
2024-03-17 13:50:21 +0800

    gawa kagabi

---
+++ src/views/Session.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted, reactive, ref, watch } from 'vue';
+
+</script>
+
+<template>
+
+    <div class="flex flex-row gap-2 relative">
+
+        <Fieldset class="flex-auto flex-row" unstyled style="height: 80vh;">
+            <template #legend>
+                <div class="flex align-items-center pl-2">
+                    <i class="pi pi-cart-plus mr-2"> </i>
+                    <span class="font-bold text-xl">Session</span>
+                </div>
+            </template>
+            <div class="absolute top-0 right-0 p-4 ">
+                <Button icon="pi pi-plus" label="New Session">
+                </Button>
+            </div>
+        </Fieldset>
+    </div>
+</template>
